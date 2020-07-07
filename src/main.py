b85ad9f3ab2f4c2835921bfaf5a13dffc082cff8
Feras Maali <ferasn.maali@gmail.com>
2020-07-07 15:08:33 +0300

    Made release notes building almost dynamic

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import datetime
 import logging
 import yaml
 import json
@@ -14,11 +15,22 @@ def main():
     global args
     args = get_arguments()
 
-    data = load_yaml(args.input_file)
+    # create destination directory
     os.mkdir(args.output_directory, mode=0o744)
-    write_manifest(data['manifest'])
-    write_release_notes(data['release_notes'])
-    move_rpms()
+
+    # move rpm files to destination directory
+    rpm_list = move_rpms()
+
+    # read the manifest from the yaml file
+    manifest = load_yaml(args.input_file)
+
+    # write manifest to a json file
+    write_manifest(manifest)
+
+    # extract release notes and write them to their file
+    write_release_notes(manifest, rpm_list)
+
+    # compress the folder to produce the result
     compress_folder(args.output_file, args.output_directory)
 
     if args.clean:
@@ -27,12 +39,16 @@ def main():
 
 
 def move_rpms():
-    for xfile in os.listdir(args.rpms_directory):
+    rpm_list = os.listdir(args.rpms_directory)
+
+    for xfile in rpm_list:
         src = os.path.join(args.rpms_directory, xfile)
         dest = os.path.join(args.output_directory, xfile)
         shutil.copyfile(src, dest)
 
     logging.debug('moved rpms to output directory')
+
+    return rpm_list
 
 
 def compress_folder(output_file, output_directory):
@@ -41,10 +57,34 @@ def compress_folder(output_file, output_directory):
     logging.debug('finished compressing {0}'.format(args.output_file))
 
 
-def write_release_notes(data):
+def write_release_notes(data, rpm_list):
     with open(os.path.join(args.output_directory, args.release_notes_file), 'w') as output_file:
-        output_file.write(data)
+        # Writing patch fixes to the file
+        output_file.write('The CVM_PE_GI-7.7r1.7.1-'
+                          + f'{datetime.datetime.now():%Y%m%d}'
+                          + ' patch fixes the following CVEs:\n')
+        for fix in get_patch_fixes(data):
+            output_file.write('  ' + fix + '\n')
+
+        # Writing rpms list to the release_notes file
+        output_file.write('\nNew patches included in this bundle:\n')
+        for rpm in rpm_list:
+            output_file.write('  ' + rpm + '\n')
+
+        # Writing targeted releases
+        output_file.write('\nTargeted releases:\n')
+        for release in get_targeted_releases(data):
+            output_file.write('  ' + release + '\n')
+
     logging.debug('release_notes file was written successfully')
+
+
+def get_patch_fixes(data):
+    return [x['CESA'] for x in data['CESA_list']]
+
+
+def get_targeted_releases(data):
+    return ['7.7r1.7', '7.7r1.6', '7.7r1.5.1', '7.6r2']
 
 
 def write_manifest(data):
@@ -55,9 +95,9 @@ def write_manifest(data):
 
 def init_logger():
     logging.basicConfig(
-                    level=logging.DEBUG,
-                    format='%(asctime)s %(name)s.%(funcName)-15s +%(lineno)s: %(levelname)-8s %(message)s'
-                )
+            level=logging.DEBUG,
+            format='%(asctime)s %(name)s.%(funcName)-15s +%(lineno)s: %(levelname)-8s %(message)s'
+        )
     logging.debug('logging started working')
 
 
@@ -79,24 +119,24 @@ def get_arguments():
     parser.add_argument(
         '--output-file',
         help='Output file name',
-        default='CVM_PE_GI-7.7r1.6.1-20200303-x86_64.tar.gz'
+        default='CVM_PE_GI-7.7r1.6.1-' + f'{datetime.datetime.now():%Y%m%d}' + '-x86_64.tar.gz'
     )
 
     parser.add_argument(
         '--output-directory',
         help='Output directory name',
-        default='CVM_PE_GI-7.7r1.6.1-20200303-x86_64'
-    )
-
-    parser.add_argument(
-        '--manifest-file',
-        help='manifest file name',
-        default='CVM_PE_GI-7.7r1.7.1-20200319-x86_64.release_notes'
+        default='CVM_PE_GI-7.7r1.6.1-' + f'{datetime.datetime.now():%Y%m%d}' + '-x86_64'
     )
 
     parser.add_argument(
         '--release-notes-file',
         help='release notes file name',
+        default='CVM_PE_GI-7.7r1.7.1-' + f'{datetime.datetime.now():%Y%m%d}' + '-x86_64.release_notes'
+    )
+
+    parser.add_argument(
+        '--manifest-file',
+        help='manifest file name',
         default='CVM_RPM_LIST_MANIFEST.json'
     )
 
